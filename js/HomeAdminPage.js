9f2da7afd0936b8b3075abca3d97c51dd476b5fd
liam <newtonliam03@gmail.com>
2024-04-30 10:28:12 +1000

    New Branch for Front End

---
+++ js/HomeAdminPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import { Link } from 'react-router-dom';
+
+
+function HomeAdminPage() {
+  return (
+    <div>
+      <h1>This is the Home Admin Page</h1>
+      <p>This is the content of the new page.</p>
+    </div>
+  );
+}
+
+export default HomeAdminPage;
