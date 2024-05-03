6489895c122dac598af756887cf3543f5da64201
liam <newtonliam03@gmail.com>
2024-05-03 10:34:36 +1000

    New main

---
+++ NodeTest.js
@@ -0,0 +1,10 @@
+
+// Define a route for the homepage
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
