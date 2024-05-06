4165836c7366d7b0c65b6d07db5ab6fc01a3e0be
liam <newtonliam03@gmail.com>
2024-05-06 23:52:38 +1000

    Show data button

--- my-react-app/src/Components/AddPage.js
+++ my-react-app/src/Components/AddPage.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './Navbar'; // Import your Navbar component
-import './HomeAdminPage.css' // Use HomeAdminPage CSS for styling
+import './HomeAdminPage.css'; // Use HomeAdminPage CSS for styling
+import './AddPage.css'
 
 function AddPage() {
+    const [showData, setShowData] = useState(false);
+
+    const toggleData = () => {
+        setShowData(!showData);
+    };
+
     return (
         <div>
             <Navbar />
@@ -10,7 +17,21 @@ function AddPage() {
                 <img src="/logo.png" alt="Company Logo" className="company-logo" />
                 <h1>This is the Add Page</h1>
             </div>
-            <p>This is the content of the new page.</p>
+            <div className="button-container">
+                <button className="show-button" onClick={toggleData}>
+                    {showData ? "Hide Data" : "Click to show data"}
+                </button>
+            </div>
+            {showData && (
+                <div className="data-container">
+                    {/* Your data goes here */}
+                    {/* For example, you can display a table or any other content */}
+                    <textarea className="data-textarea" readOnly>
+                        John - 30
+                        Jane - 25
+                    </textarea>
+                </div>
+            )}
         </div>
     );
 }
