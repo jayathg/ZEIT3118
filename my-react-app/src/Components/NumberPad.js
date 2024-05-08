75c6956cece6f80b02613d31f5538463cbec82b3
Jayath Gunawardena <jayathg1@gmail.com>
2024-05-08 21:43:53 +1000

    Use https

--- my-react-app/src/Components/NumberPad.js
+++ my-react-app/src/Components/NumberPad.js
@@ -15,7 +15,7 @@ function NumberPad() {
   const navigateToHomeAdminPage = async () => {
     console.log("Navigating to Home Admin Page")
     try {
-      const response = await axios.post(`http://techsecuretaskforce.azurewebsites.net/login`, { userID: employeeID });
+      const response = await axios.post(`https://techsecuretaskforce.azurewebsites.net/login`, { userID: employeeID });
       console.log("Login response:", response.data);
       //navigate('/HomeAdminPage'); // Adjust route as necessary
     } catch (error) {
