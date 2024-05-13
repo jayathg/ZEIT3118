ae6e4d392960ad4a449b4a8d7df4dad2f3ae3e3c
Jayath Gunawardena <jayathg1@gmail.com>
2024-05-13 10:20:20 +1000

    Fix bug in login functionality

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
-import './Components/Navbar.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+
 root.render(
+<Auth0Provider
+    domain="dev-we3vguqrc7tyu1mr.us.auth0.com"
+    clientId="1nzOnOcVNNFCtzB7CxXV87MpTL6IGb97"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
+  </Auth0Provider>,
 );
