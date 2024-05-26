607929222771551834822fb5ed3a6258c2c871ff
Jayath Gunawardena <jayathg1@gmail.com>
2024-05-26 15:01:19 +1000

    Fix spelling

--- my-react-app/src/Components/DeletePage.js
+++ my-react-app/src/Components/DeletePage.js
@@ -34,7 +34,7 @@ function DeletePage() {
                 
                 // Now you can use the users array as needed
             users.forEach(user => {
-                    onsole.log(`UserID: ${user.userID}, User: ${user.firstName}, ${user.lastName}, Email: ${user.email}`);
+                    console.log(`UserID: ${user.userID}, User: ${user.firstName}, ${user.lastName}, Email: ${user.email}`);
             });
     
                 // You can also set this data to the state if you want to display it in the UI
