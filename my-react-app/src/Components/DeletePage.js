5aa7d56814d203b2b04668778bad17c696112a5f
Jayath Gunawardena <jayathg1@gmail.com>
2024-05-26 15:49:54 +1000

    Update deleteUser API endpoint in DeletePage.js

--- my-react-app/src/Components/DeletePage.js
+++ my-react-app/src/Components/DeletePage.js
@@ -48,7 +48,7 @@ function DeletePage() {
     const deleteUser = async (userId) => {
         console.log("Deleting User:", userId);
         try {
-            const response = await axios.delete(`https://techsecuretaskforcefunction.azurewebsites.net/api/deleteUser?userId=${userId}`);
+            const response = await axios.delete(`https://techsecuretaskforcefunction.azurewebsites.net/api/httpTrigger3?userId=${userId}`);
             console.log("Delete response:", response.data);
             setPopupMessage(`User ${userId} has been deleted.`);
             // Optionally update dummyData to remove the deleted user
