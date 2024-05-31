1d34db6d9dbfad00e16f1b28e287c6b9eb901e23
Jayath Gunawardena <jayathg1@gmail.com>
2024-05-31 12:18:25 +1000

    use Navigate

--- my-react-app/src/Components/AuthCallback.js
+++ my-react-app/src/Components/AuthCallback.js
@@ -1,15 +1,16 @@
 import React, { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 
 const AuthCallback = () => {
-    const history = useHistory();
+    const navigate = useNavigate(); // Hook for navigation
+
 
     useEffect(() => {
         // Simulate setting user role based on admin authentication
         const userRole = 'admin'; // Assume the user is an admin for this example
         Cookies.set('userRole', userRole, { expires: 1 }); // Set cookie with role
-        history.push('/HomeAdminPage');
+        navigate('/HomeAdminPage');
         
     }, [history]);
 
