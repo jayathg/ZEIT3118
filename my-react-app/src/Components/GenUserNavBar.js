04e444698f5808633e9ce8fbfc171584f12b8420
Jayath Gunawardena <jayathg1@gmail.com>
2024-05-31 13:32:19 +1000

    Add navigation for GenUserNavBar

--- my-react-app/src/Components/GenUserNavBar.js
+++ my-react-app/src/Components/GenUserNavBar.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Use Link from react-router-dom
+import { Link, useNavigate } from 'react-router-dom'; // Use Link from react-router-dom
 import './Navbar.css'; // Use the same CSS file for styling
 
+
 function SimpleNavbar() {
+    const navigate = useNavigate(); // Hook for navigation
+
     const navigateToLogoutPage = () => {
         navigate('/Logout')
     }
